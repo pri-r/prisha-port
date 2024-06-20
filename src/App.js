44d8beb657a9f6ed5a59d8c1564d5d2514604645
Prisha <prisha.rao@student.unsw.edu.au>
2024-06-20 10:32:34 +1000

    tryign to fix stuff

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const AppContent = () => {
           <Route path="/prisha-port/" element={<Main />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contactme" element={<Contact />} />
-          <Route path="*" element={<Navigate to="/prisha-port/" />} />
+          <Route path="*" element={<Main />} />
         </Routes>
         {showImage && (
           <div className="image-container">
